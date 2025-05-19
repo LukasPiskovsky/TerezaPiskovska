8b64d5bfd0c02a4ecaeb13e9398a3e7ebc8149e2
LukasPiskovsky <lukaspiskovsky@gmail.com>
2025-05-19 19:39:45 +0200

    update

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ const serviceP = document.querySelectorAll(".service-p")
 
 
 //vh
-// function setRealVh() {
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-//     }
-//     setRealVh();
-// window.addEventListener('resize', setRealVh);
+function setRealVh() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setRealVh();
+window.addEventListener('resize', setRealVh);
 
 
 //Menu
